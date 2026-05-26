f5fab4a0b2ce39dba1c561a894f18477b8170eac
hectorOrtiz <transtornador@gmail.com>
2026-05-26 17:21:45 -0600

    lleando

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
             <p>Texto</p>
             <nav>
                 <a href="https://www.gob.mx/aviso_de_privacidad" target="_blank" >Aviso de Privacidad |</a>
-                <a href="INSERTAR URL DE FACEBOOK" target="_blank">Facebook |</a>
+                <a href="INSERTAR URL DE FACEBOOK" target="_blank">Facebo123ok |</a>
                 <a href="INSERTAR URL DE TIKTOK" target="_blank">TikTok |</a>
                 <a href="INSERTAR URL DE YOUTUBE" target="_blank">Youtube |</a>
                 <a href="https://wa.me/INSERTAR_NUMERO_DE_WHATSAPP" target="_blank">WhatsApp</a>
